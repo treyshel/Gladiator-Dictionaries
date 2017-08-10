29ee982ccf7f0c2a1718b43ae0484d22533754b8
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-08-10 15:09:54 -0500

    made the is dead function

--- core.py
+++ core.py
@@ -5,3 +5,12 @@ def new_gladiator(health, rage, damage_low, damage_high):
         'damage_low': damage_low,
         'damage_high': damage_high
     }
+
+
+# def attack(attacker, defender):
+
+
+def is_dead(gladiator):
+    if ['health'] <= 0:
+        return True
+    return False
