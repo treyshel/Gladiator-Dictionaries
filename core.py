557b02c7d09686bd797316c92cd5345f3b7cdbcd
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-08-10 14:56:11 -0500

    made my new gladiator function

---
+++ core.py
@@ -0,0 +1,7 @@
+def new_gladiator(health, rage, damage_low, damage_high):
+    return {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
